9ec3e8fe678f4dbd52303f1aef86205a7671cb3f
uday701 <udaykiran701381@gmail.com>
2023-04-22 16:08:45 +0530

    changed config info in client

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,4 @@
-let BASE_URL = "http://localhost:4000/";
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
-}
+let BASE_URL = "https://quora-clone-uday.onrender.com/";
+
 
 export { BASE_URL };
